78032d1b548fcdfbc6b800a9d8eb89b44a2d2677
nahiyan <nahiyan.alamgir@northsouth.edu>
2019-12-14 19:36:39 +0600

    Add admin authorization.

---
+++ app/Http/Middleware/AuthAdmin.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class AuthAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $guard = null)
+    {
+        if (Auth::guard($guard)->check() && Auth::user()->type == "admin") {
+            return $next($request);
+        } else {
+            return redirect("/")->with("danger", "You need to be logged in as an admin to access this page");
+        }
+
+    }
+}
